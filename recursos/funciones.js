2ba6c6c6da1c4d63d774753ab603b13b4296a00d
Carlos Fadul <carlosfadul@gmail.com>
2022-03-02 14:52:25 -0500

    otra

--- recursos/funciones.js
+++ recursos/funciones.js
@@ -31,7 +31,7 @@ function Validar() {
     document.getElementById("txtMayor15").value = "Si";
   else
     document.getElementById("txtMayor15").value = "No";
-  documento.getElementById("txtEdad").value = edad;
+  documento.getElementById("txtEdad").value = edad.toString();
   let mayor15 = document.querySelector("#txtMayor15").value;
   let colombiano = document.querySelector("#txtColombiano").value;
   let bachiller = document.querySelector("#txtBachiller").value;
@@ -97,7 +97,7 @@ function BuscarBeneficiario() {
       if (mensaje != "")
         alert("Este beneficiario ya fue procesado")
       else {
-        alert("Beneficiario agregado a la base de datos")
+       
         RegistrarBeneficiario()
       }
     })
